26b25a2b4ec60c73fed52a30e1153d1abf7b9276
Hanage999 <hanage999@crazynewworld.net>
2021-05-03 10:43:51 +0900

    Respond to unattended notifications on waking up

---
+++ database_tables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `bots` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) NOT NULL DEFAULT '',
+  `checked_until` int(11) unsigned NOT NULL DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `bot_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `song_candidates`
+(
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `bot_id` int(11) unsigned NOT NULL,
+  `item_id` int(11) unsigned NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `songs` varchar(2000) DEFAULT '',
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `item_per_bot` (`bot_id`,`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `items` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `url` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `content` text NOT NULL,
+  `summary` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `rss_feeds` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(191) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

0ec4626db91982a009a1f668a6937ed3cf3591a5
Hanage999 <hanage999@crazynewworld.net>
2023-11-25 18:49:09 +0900

    Move TZF finder to a global variable

--- tankabot.go
+++ tankabot.go
@@ -9,11 +9,13 @@ import (
 	"time"
 
 	"github.com/comail/colog"
+	"github.com/ringsaturn/tzf"
 	"github.com/spf13/viper"
 )
 
 var (
 	version = "1"
+	f       tzf.F
 )
 
 type commonSettings struct {
@@ -144,6 +146,13 @@ func Initialize() (bot Persona, db DB, err error) {
 		return bot, db, err
 	}
 	bot.DBID = id
+
+	// TZF（グローバル変数に設定）を初期化
+	f, err = tzf.NewDefaultFinder()
+	if err != nil {
+		log.Printf("info: %s", err)
+		return bot, db, err
+	}
 
 	// botの住処を登録
 	if bot.LivesWithSun {
